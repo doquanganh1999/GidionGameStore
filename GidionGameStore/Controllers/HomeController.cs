a40180c2b5efe77c8c4084dbc9cfdfb959e7452b
Gad <Gad@DESKTOP-PQN0LPD>
2026-08-30 22:56:52 +0700

    Add project files.

---
+++ GidionGameStore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using GidionGameStore.Models;
+using GidionGameStore.Models.GameStoreDSTableAdapters;
+using System.Web.Mvc;
+
+namespace GidionGameStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            GameStoreDS gameStoreDS = new GameStoreDS();
+            GameInformationTableAdapter gameInformationTableAdapter = new GameInformationTableAdapter();
+            gameInformationTableAdapter.Fill(gameStoreDS.GameInformation);
+            ViewBag.GameInformation = gameStoreDS.GameInformation;
+            ViewBag.GamesCount = gameStoreDS.GameInformation.Count;
+            ViewBag.ErrorMessage = gameInformationTableAdapter.Connection.State == System.Data.ConnectionState.Open ? null : "Database connection is not open.";
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ GidionGameStore/Controllers/PlayGameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GidionGameStore.Controllers
+{
+    public class PlayGameController : Controller
+    {
+        // GET: PlayGame
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GuessSong()
+        {
+            return View();
+        }
+    }
+}
